966a8fd7a70a1925f6681907cac1994f83f14fb1
Jared Delora-Ellefson <jareddelora@gmail.com>
2020-07-04 10:15:32 -0400

    pushing

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import KEY
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = KEY
+app.config['SECRET_KEY'] = KEY.S_Key().KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='defaul.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
